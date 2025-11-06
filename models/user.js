8402ec7823f687b9337e063a58a87a2b23cf4597
Lalu Fatan <lalufatan25@gmail.com>
2025-11-06 11:51:43 +0700

    ganti kode user js

--- models/user.js
+++ models/user.js
@@ -4,17 +4,34 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   }
   User.init({
-    nama: DataTypes.STRING
+    nama: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('mahasiswa', 'admin'),
+      allowNull: false,
+      defaultValue: 'mahasiswa',
+      validate: {
+        isIn: [['mahasiswa', 'admin']]
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
